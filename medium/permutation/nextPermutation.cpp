dc6bb94a4375cf4e3fb2db01aea02c84636fca2c
0123sandeepkumar <speedan0123@gmail.com>
2023-08-26 23:52:50 +0530

    next permutation

---
+++ medium/permutation/nextPermutation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+void nextPermutation(std::vector<int>& nums) {
+    int n = nums.size();
+    int i = n - 2;
+
+    // Find the first decreasing element starting from the right
+    while (i >= 0 && nums[i] >= nums[i + 1]) {
+        i--;
+    }
+
+    if (i >= 0) {
+        // Find the smallest element to the right of nums[i] that is larger than nums[i]
+        int j = n - 1;
+        while (nums[j] <= nums[i]) {
+            j--;
+        }
+
+        // Swap nums[i] and nums[j]
+        std::swap(nums[i], nums[j]);
+    }
+
+    // Reverse the elements after index i
+    std::reverse(nums.begin() + i + 1, nums.end());
+}
+
+int main() {
+    std::vector<int> nums = {1, 2, 3};
+
+    nextPermutation(nums);
+
+    for (int num : nums) {
+        std::cout << num << " ";
+    }
+
+    return 0;
+}
